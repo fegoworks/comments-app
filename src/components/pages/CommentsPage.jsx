07a2d9a147a06bd52c4ab9dbef8488c9f273cc7a
unknown <fegoworks@hotmail.com>
2021-03-14 12:37:36 +0100

    chore(pageComponents): initial draft

---
+++ src/components/pages/CommentsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageTemplate from '../UI/templates/PageTemplate'
+
+
+const CommentsPage = () => (
+  <PageTemplate>
+    na we dey here
+  </PageTemplate>
+)
+
+export default CommentsPage
---
+++ src/components/pages/RepliesPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const RepliesPage = () => (
+  <div></div>
+)
+
+export default RepliesPage
