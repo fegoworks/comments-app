4dea92b17abb50472b3eb4bf8535b319fdb5ffad
unknown <fegoworks@hotmail.com>
2021-03-15 23:44:14 +0100

    chore(ApiKey): include key constant

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios'
-import { BASE_API_URL } from '<constants>/constants'
+import {BASE_API_URL} from '<constants>/constants'
+import { API_KEY } from '../constants/constants';
+
 
 const api = axios.create({
   baseURL: BASE_API_URL,
 })
+
+api.defaults.headers.get['key'] = API_KEY;
 
 export default api
