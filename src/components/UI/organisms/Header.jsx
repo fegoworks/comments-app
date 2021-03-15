19be893a8651bbe2449a907255c7e3ee47cc6059
unknown <fegoworks@hotmail.com>
2021-03-16 00:09:28 +0100

    feat(header): build out component

--- src/components/UI/organisms/Header.jsx
+++ src/components/UI/organisms/Header.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
+import styled from 'styled-components'
+import Text from '<atoms>/Text'
+import { textColors } from '<variables>/colorPalette'
+import { spacing } from '<variables>/spacing'
 
 const Header = () => (
-  <div>Header</div>
+  <Header.Navigation>
+    <div className="left">
+      <Text
+        fontSize="base"
+        textAlign="center"
+        color="white"
+        fontWeight="semiBold"
+        padding={spacing["xs"]}
+        margin= "0"
+      >
+        MiniComments
+      </Text>
+    </div>
+    <div className="right">
+      <ul>
+        <li>Articles</li>
+        <li>Comment</li>
+      </ul>
+    </div>
+  </Header.Navigation>
 )
+
+Header.Navigation = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 2rem;
+
+  .left {
+    display: flex;
+    align-items: center;
+
+  }
+
+  .right {
+    ul {
+      display: flex;
+      li {
+        list-style: none;
+        margin: 0 2rem;
+        color: ${textColors['secondary']}
+      }
+    }
+  }
+`
 
 export default Header
