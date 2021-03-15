70ad5009ae2b8ab6f7afc065cb92ef1ad2f0144c
unknown <fegoworks@hotmail.com>
2021-03-16 00:07:18 +0100

    feat(commentsPage): build out comments page

--- src/components/pages/CommentsPage.jsx
+++ src/components/pages/CommentsPage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import PageTemplate from '../UI/templates/PageTemplate'
+import React, {useContext} from 'react'
+import CommentsList from '<organisms>/CommentsList'
+import PageTemplate from '<templates>/PageTemplate'
+import ReactLoading from 'react-loading'
+import {CommentsContext} from '../../state/GlobalContext'
 
 
-const CommentsPage = () => (
-  <PageTemplate>
-    na we dey here
-  </PageTemplate>
-)
+const CommentsPage = () => {
+  const {comments, loading, error} = useContext(CommentsContext)
+  let pageContent
+  if (error) {
+    pageContent = <h2>{error.toString()}</h2>
+  } else if (loading) {
+    pageContent = <ReactLoading type='spinningBubbles' color="#b19cd9" height={667} width={375}/>
+  } else if (!loading && comments) {
+    pageContent =
+    (<CommentsList commentsList = {comments} />)
+  }
+  return (
+    <PageTemplate>
+      {pageContent}
+    </PageTemplate>
+  )
+}
 
 export default CommentsPage
